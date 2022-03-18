bf0cf57dcd9b332dd680e642c69e1f1f998595b8
TruongVanPhon <truongvanphon020297@gmail.com>
2022-03-18 15:10:08 +0700

    add folder Face_Book 15h10

---
+++ Face_Book/Home.js
@@ -0,0 +1,24 @@
+function Show(){
+   let info_value =  document.getElementById("abc");
+   info_value.style.display = "block";
+   let screen_value = document.getElementById("cdf");
+   screen_value.style.display = "block";
+}
+function Screen_off() {
+   let info_value =  document.getElementById("abc");
+   info_value.style.display = "none";
+   let screen_value = document.getElementById("cdf");
+   screen_value.style.display = "none";
+}
+function Show_info() {
+   let info_value =  document.getElementById("phon");
+   info_value.style.display = "block";
+   let screen_value = document.getElementById("cdf");
+   screen_value.style.display = "block";
+}
+function Info_off() {
+   let info_value =  document.getElementById("phon");
+   info_value.style.display = "none";
+   let screen_value = document.getElementById("cdf");
+   screen_value.style.display = "none";
+}
